791046c79f9eaf3f4340ea2dc977c4c91e11fced
joelcgoddard <76084602+joelcgoddard@users.noreply.github.com>
2021-04-03 12:14:04 +1030

    Add files via upload

--- Top_Menu.java
+++ Top_Menu.java
@@ -5,43 +5,73 @@ import java.util.Scanner;
 public class Top_Menu {
 
 	public static void main(String[] args) {
-		
-		System.out.println("Select an option:");
-		System.out.println("1. Register a new account");
-		System.out.println("2. Log In");
-		
-		// take input 
-		 Scanner in = new Scanner(System.in);
-		
-		int menuSelection = in.nextInt();
-		
-		// process input
-		switch(menuSelection) {
-		
-		case 1: // Register for an account
-			System.out.println("Input your details to register:");
-			
-			// run account register method
-			
-			break;
-			
-		case 2: // log in, go to logged in menu
-			System.out.println("Logging you in.....");
-			
-			// run logged in menu
-			
-			break;
-			
-		default:
 
-            System.out.println("Invalid choice.");
-			
+		// boolean to quit the menu loop
+		boolean quit = false;
+
+		// making the scanner to take input
+		Scanner in = new Scanner(System.in);
+
+		// starting the menu loop
+		while (quit == false) {
+
+			System.out.println("Select an option:");
+			System.out.println("1. Register a new account");
+			System.out.println("2. Log In");
+			System.out.println("3. Quit");
+
+			// take input
+			int menuSelection = in.nextInt();
+
+			// process input
+			switch (menuSelection) {
+
+			case 1: // Register for an account
+				System.out.println("Input your details to register:");
+
+				// run account register method
+				registerAccount();
+
+				break;
+
+			case 2: // log in, go to logged in menu
+				System.out.println("Log in:");
+
+				// run logged in method
+				logIn();
+
+				break;
+
+			case 3: // quit the menu and terminate
+				System.out.println("Quitting...");
+
+				quit = true;
+				break;
+
+			default:
+
+				System.out.println("Invalid choice.");
+
+			}
+
 		}
 		
 		in.close();
-		
-		
-
 	}
 
+	// account register method
+	public static void registerAccount() {
+		System.out.println("Enter your email address:");
+	}
+
+	// run logged in method (ask for password)
+	public static void logIn() {
+		System.out.println("Enter your email address:");
+		System.out.println("Enter your password:");
+
+		// check log in details
+
+		// if details are correct, log in and go to log in menu
+
+	}
 }
