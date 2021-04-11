8b643a265e99420dd7e8887d84b37de6dec4f94a
joelcgoddard <76084602+joelcgoddard@users.noreply.github.com>
2021-04-11 16:37:59 +0930

    Update Logged_In_Menu.java

--- Logged_In_Menu.java
+++ Logged_In_Menu.java
@@ -3,16 +3,16 @@ package Menues;
 import java.util.Scanner;
 
 public class Logged_In_Menu {
+	private static final Scanner sc = new Scanner(System.in);
+	private static final Submit_IT_Docket[] itsubmittions = new Submit_IT_Docket[1000];
+
+	private static int submittionCount = 0;
 
 	public static void main(String[] args) {
+		String menuSelection;
+		char selection = '\0';
 
-		// boolean to quit the menu loop
-		boolean quit = false;
-
-		// take input
-		Scanner in = new Scanner(System.in);
-
-		while (quit == false) {
+		do {
 
 			System.out.println("Select an option:");
 			System.out.println("1. Submit an IT ticket");
@@ -20,54 +20,98 @@ public class Logged_In_Menu {
 			System.out.println("3. Log Out");
 			System.out.println("4. Change Password");
 
-			int menuSelection = in.nextInt();
+			// take input
+			System.out.println();
 
-			// process input
-			switch (menuSelection) {
+			System.out.print("Enter your selection: ");
+			menuSelection = sc.nextLine();
 
-			case 1: // submit an IT ticket
-				System.out.println("Input Ticket Details");
+			if (menuSelection.length() != 1) {
+				System.out.println("Error - selection must be a single character!");
 
-				// run ticket submission method
+			} else {
+				selection = Character.toUpperCase(menuSelection.charAt(0));
 
-				break;
+				switch (selection) {
+				case '1':
 
-			case 2: // View IT ticket submissions
-				System.out.println("Here are your current ticket submissions:");
+					addITsubmittion();
+					break;
 
-				// run IT ticket list method
+				case '2':
 
-				break;
+					listAll();
+					break;
 
-			case 3: // Log Out
-				System.out.println("Logging you out.....");
-				System.out.println(".....");
+				case '3':
 
-				// run log out method
-				logOut();
+					System.out.println("Logging you out – goodbye!");
+					// run log out method
+					logOut();
 
-				break;
+					break;
 
-			case 4: // Change Password
-				System.out.println("Change your password:");
+				case '4':
 
-				// run change password method
+					changePassword();
+					break;
 
-				break;
+				default:
 
-			default:
-
-				System.out.println("Invalid choice.");
-
+					System.out.println("Error - invalid selection!");
+				}
 			}
+			System.out.println();
 
-		}
+		} while (selection != 'X');
+
 	}
-	
-	
+
+	private static void changePassword() {
+		// TODO Auto-generated method stub
+		System.out.println("Change your password:");
+	}
+
+	private static void listAll() {
+		// TODO Auto-generated method stub
+		System.out.println("Here are your current ticket submissions:");
+
+		for (int i = 0; i < submittionCount; i++) {
+			itsubmittions[i].printITsubmittion();
+			System.out.println();
+		}
+
+	}
+
+	private static void addITsubmittion() {
+		// TODO Auto-generated method stub
+		System.out.println("Input Ticket Details");
+
+		String description;
+		int severity;
+
+		System.out.println("Please Enter desciption of issue:");
+		description = sc.nextLine();
+		System.out.println("Please select severy:");
+		severity = sc.nextInt();
+
+		sc.nextLine();
+
+		itsubmittions[submittionCount] = new Submit_IT_Docket(description, severity);
+		submittionCount++;
+
+		sc.nextLine();
+
+		for (int i = 0; i < submittionCount; i++) {
+			itsubmittions[i].printITsubmittion();
+			System.out.println();
+			System.out.println("IT docket has been successfully submitted to the IT team - Thank you");
+		}
+
+	}
+
 	// log out method
-	public static void logOut()
-	{
+	public static void logOut() {
 		Top_Menu.main(null);
 	}
 
