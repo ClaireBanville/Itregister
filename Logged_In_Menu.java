87c7fbb26f734551cbe0d6aca93bf8e92d371593
ClaireBanville <48041048+ClaireBanville@users.noreply.github.com>
2021-04-11 19:51:33 +1000

    Update Logged_In_Menu.java

--- Logged_In_Menu.java
+++ Logged_In_Menu.java
@@ -2,117 +2,112 @@ package Menues;
 
 import java.util.Scanner;
 
+
 public class Logged_In_Menu {
 	private static final Scanner sc = new Scanner(System.in);
 	private static final Submit_IT_Docket[] itsubmittions = new Submit_IT_Docket[1000];
-
 	private static int submittionCount = 0;
-
+	
 	public static void main(String[] args) {
-		String menuSelection;
-		char selection = '\0';
+	String menuSelection;
+	char selection = '\0';
+	
 
-		do {
+	do 	{		
+		System.out.println("Select an option:");
+		System.out.println("1. Submit an IT ticket");
+		System.out.println("2. View IT ticket submissions");
+		System.out.println("3. Log Out");
+		System.out.println("4. Change Password");
+		
+		// take input 
+		System.out.println();
 
-			System.out.println("Select an option:");
-			System.out.println("1. Submit an IT ticket");
-			System.out.println("2. View IT ticket submissions");
-			System.out.println("3. Log Out");
-			System.out.println("4. Change Password");
+		System.out.print("Enter your selection: ");
+		menuSelection = sc.nextLine();
 
-			// take input
-			System.out.println();
+		if (menuSelection.length() != 1) {
+			System.out.println("Error - selection must be a single character!");
 
-			System.out.print("Enter your selection: ");
-			menuSelection = sc.nextLine();
+		} else {
+			selection = Character.toUpperCase(menuSelection.charAt(0));
 
-			if (menuSelection.length() != 1) {
-				System.out.println("Error - selection must be a single character!");
+			switch (selection) {
+			case '1':
 
-			} else {
-				selection = Character.toUpperCase(menuSelection.charAt(0));
+				addITsubmittion();
+				break;
 
-				switch (selection) {
-				case '1':
+			case '2':
 
-					addITsubmittion();
-					break;
+				listAll();
+				break;
 
-				case '2':
+			case '3':
 
-					listAll();
-					break;
+				System.out.println("System shutting down – goodbye!");
+				Top_Menu.main(null);
+				break;
 
-				case '3':
+			case '4':
 
-					System.out.println("Logging you out – goodbye!");
-					// run log out method
-					logOut();
+				changePassword();
+				break;
+				
+			default:
 
-					break;
-
-				case '4':
-
-					changePassword();
-					break;
-
-				default:
-
-					System.out.println("Error - invalid selection!");
-				}
+				System.out.println("Error - invalid selection!");
 			}
-			System.out.println();
-
-		} while (selection != '3');
-
-	}
-
-	private static void changePassword() {
-		// TODO Auto-generated method stub
-		System.out.println("Change your password:");
-	}
-
-	private static void listAll() {
-		// TODO Auto-generated method stub
-		System.out.println("Here are your current ticket submissions:");
-
-		for (int i = 0; i < submittionCount; i++) {
-			itsubmittions[i].printITsubmittion();
-			System.out.println();
 		}
+		System.out.println();
 
-	}
+	} while (selection != '3');
 
-	private static void addITsubmittion() {
+}
+
+		 private static void changePassword() {
 		// TODO Auto-generated method stub
-		System.out.println("Input Ticket Details");
-
-		String description;
-		int severity;
-
-		System.out.println("Please Enter desciption of issue:");
-		description = sc.nextLine();
-		System.out.println("Please select severy:");
-		severity = sc.nextInt();
-
-		sc.nextLine();
-
-		itsubmittions[submittionCount] = new Submit_IT_Docket(description, severity);
-		submittionCount++;
-
-		sc.nextLine();
-
-		for (int i = 0; i < submittionCount; i++) {
-			itsubmittions[i].printITsubmittion();
-			System.out.println();
-			System.out.println("IT docket has been successfully submitted to the IT team - Thank you");
-		}
-
+				System.out.println("Change your password:");
 	}
 
-	// log out method
-	public static void logOut() {
-		Top_Menu.main(null);
+		private static void listAll() {
+		// TODO Auto-generated method stub
+			System.out.println("Here are your current ticket submissions:");
+			
+			for (int i = 0; i < submittionCount; i++) {
+				itsubmittions[i].printITsubmittion();
+				System.out.println();
+			}
+		
+	}
+
+		private static void addITsubmittion() {
+		// TODO Auto-generated method stub
+			System.out.println("Input Ticket Details");
+			
+			String description;
+			int severity;
+
+			System.out.println("Please Enter desciption of issue:");
+			description = sc.nextLine();
+			System.out.println("Please select severy:");
+			severity = sc.nextInt();
+						
+			sc.nextLine();
+
+			itsubmittions[submittionCount ] = new Submit_IT_Docket(description, severity);
+			submittionCount ++;
+			
+			sc.nextLine();
+			
+			for (int i = 0; i < submittionCount; i++) {
+				itsubmittions[i].printITsubmittion();
+				System.out.println();
+
+				System.out.println("IT docket has been successfully submitted to the IT team - Thank you");
+			}
+
 	}
 
 }
+
