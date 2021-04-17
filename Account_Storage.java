398b81c3582d0e051093e06d7595133d88692cef
joelcgoddard <76084602+joelcgoddard@users.noreply.github.com>
2021-04-17 17:13:14 +0930

    Add files via upload

--- Account_Storage.java
+++ Account_Storage.java
@@ -1,11 +1,16 @@
 package accounts;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Account_Storage {
 
 	
 	// array to store users in
-	User userObjects[] = new User[10];
-	
+//	User userObjects[] = new User[10];
+
+	// creating a map of users
+	public Map<String, User> userMap = new HashMap<String, User>();
 	
 	
 	
--- User.java
+++ User.java
@@ -6,38 +6,42 @@ public class User {
 	private String password;
 	private String email;
 	private int phoneNumber;
-
-	public User(String user, String pass, String em, int no) {
-
+	
+	public User (String user, String pass, String em, int no) {
+		
 		userName = user;
 		password = pass;
 		email = em;
 		phoneNumber = no;
-
+		
 	}
-
-	public String getUser() {
-
+	
+	public String getUser(){
+		
 		return userName;
-
+		
 	}
-
-	public String getPassword() {
-
+	public String getPassword(){
+			
 		return password;
-
-	}
-
-	public String getEmail() {
-
+			
+		}
+	
+	public String getEmail(){
+		
 		return email;
-
+		
 	}
-
-	public int getPhone() {
-
+	public int getPhone(){
+		
 		return phoneNumber;
-
+		
 	}
+	
 
+	
+	
+	
+	
+	
 }
