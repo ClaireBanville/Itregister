12b1e3e6a8e69972854ab5288a8d35399398fbe0
joelcgoddard <76084602+joelcgoddard@users.noreply.github.com>
2021-04-18 21:26:08 +0930

    Add files via upload
    made methods static

--- Menu_Regex.java
+++ Menu_Regex.java
@@ -2,17 +2,19 @@ package Menues;
 
 import java.util.regex.*;
 
-public boolean checkUserName(String name) {
+public class Menu_Regex {
+
+public static boolean checkUserName(String name) {
 	
 	String regex = "^[a-zA-Z0-9._-]{3,}$";
 	
 	Pattern p = Pattern.compile(regex);
 	
-	if (name == null) {
-		
+	if(name == null)
+	{
 		return false;
-		
 	}
+	
 	
 	Matcher m = p.matcher(name);
 	  
@@ -20,68 +22,65 @@ public boolean checkUserName(String name) {
 	
 }
 
-public boolean checkPassword(String pass) {
-	
-	
-	String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
-	
-	Pattern p = Pattern.compile(regex);
-	
-	if (pass == null) {
-		
-		return false;
-		
-	}
-	
-	Matcher m = p.matcher(pass);
-	  
-    return true;
-	
-	
-}
+	public static boolean checkPassword(String pass) {
 
-public boolean checkEmail(String email) {
-	
-	
-	String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"
-			+ "\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\"
-			+ "x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
-			+ "\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9]"
-			+ "[0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])"
-			+ "|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\"
-			+ "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
-	
-	Pattern p = Pattern.compile(regex);
-	
-	if (email == null) {
-		
-		return false;
-		
-	}
-	
-	Matcher m = p.matcher(email);
-	  
-    return true;
-	
-}
+		String regex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
 
-public boolean checkPhone(int ph) {
-	
-	
-	String regex = "^(?:\\+?(61))? ?(?:\\((?=.*\\)))?(0?[2-57-8])\\)? ?(\\d"
-			+ "\\d(?:[- ](?=\\d{3})|(?!\\d\\d[- ]?\\d[- ]))\\d\\d[- ]?\\d"
-			+ "[- ]?\\d{3})$";
-	
-	Pattern p = Pattern.compile(regex);
-	
-	if (ph == null) {
-		
-		return false;
-		
+		Pattern p = Pattern.compile(regex);
+
+		if (pass == null) {
+
+			return false;
+
+		}
+
+		Matcher m = p.matcher(pass);
+
+		return true;
+
 	}
-	
-	Matcher m = p.matcher(ph);
-	  
-    return true;
-	
+
+	public static boolean checkEmail(String email) {
+
+		String regex = "(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"
+				+ "\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\"
+				+ "x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?"
+				+ "\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9]"
+				+ "[0-9]|[1-9]?[0-9]))\\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])"
+				+ "|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\"
+				+ "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])";
+
+		Pattern p = Pattern.compile(regex);
+
+		if (email == null) {
+
+			return false;
+
+		}
+
+		Matcher m = p.matcher(email);
+
+		return true;
+
+	}
+
+	public static boolean checkPhone(CharSequence ph) {
+
+		String regex = "^(?:\\+?(61))? ?(?:\\((?=.*\\)))?(0?[2-57-8])\\)? ?(\\d"
+				+ "\\d(?:[- ](?=\\d{3})|(?!\\d\\d[- ]?\\d[- ]))\\d\\d[- ]?\\d" + "[- ]?\\d{3})$";
+
+		Pattern p = Pattern.compile(regex);
+
+		if (ph == null) {
+
+			return false;
+
+		}
+
+		Matcher m = p.matcher(ph);
+
+		return true;
+
+	}
+
 }
