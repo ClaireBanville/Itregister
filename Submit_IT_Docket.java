d08f2492c54432f74c4d393c7f1d01639a5368c9
ClaireBanville <48041048+ClaireBanville@users.noreply.github.com>
2021-04-22 18:48:06 +1000

    Update Submit_IT_Docket.java

--- Submit_IT_Docket.java
+++ Submit_IT_Docket.java
@@ -1,20 +1,25 @@
 package Menues;
 
+import java.util.Date;
+
 public class Submit_IT_Docket {
-	private String description, userName, randomValue, randomValue2;
-	private int severity;
-	private String uniqueKey;
+	private String description, userName, randomValue, randomValue2, status;
+	int severity;
+	private Date date;
+	private static String uniqueKey;
 
 	// String userName add back into array once Top menu is functional
 
-	public Submit_IT_Docket(String uniqueKey, String description, int severity, String randomValue, String randomValue2) {
+	public Submit_IT_Docket(String uniqueKey, String description, int severity, String userName, String randomValue, String randomValue2, String status, Date date) {
 		// TODO Auto-generated constructor stub
 		this.description = description;
 		this.severity = severity;
-		// this.userName = userName;
+		this.userName = userName;
 		this.uniqueKey = uniqueKey;
 		this.randomValue = randomValue;
 		this.randomValue2 = randomValue2;
+		this.status = status;
+		this.date = date;
 	}
 
 	public String getDescription() {
@@ -25,11 +30,11 @@ public class Submit_IT_Docket {
 		return severity;
 	}
 
-	// public String getuserName() {
-	// return userName;
-	// }
+	public String getuserName() {
+	return userName;
+	}
 
-	public String uniqueKey() {
+	public static String getuniqueKey() {
 		return uniqueKey;
 	}
 
@@ -40,13 +45,23 @@ public class Submit_IT_Docket {
 	public String getRandomValue2() {
 		return randomValue2;
 	}
+	public String getStatus() {
+		return status;
+	}
+	public Date getDate() {
+		return date;
+	}
+	
 	
 	public void printITsubmittion() {
 //Prints details of the submitted IT Docket
-		System.out.printf("%s %s\n", "Issue ID:", uniqueKey);
+		System.out.printf("%s %s\n", "Ticket ID: ", uniqueKey);
+		System.out.printf("%s %s\n", "Ticket Status: ", status);
 		System.out.printf("%s %s\n", "Issue Description:", description);
 		System.out.printf("%s %s\n", "Issue Severity:", severity);
-		// System.out.printf("%s %s\n", "Issue Reporter:", userName);
+		System.out.printf("%s %s\n", "Issue Reporter:", userName);
+		System.out.printf("%s %s\n", "Date Submitted:", date);
+		
 
 	}
 
@@ -54,20 +69,24 @@ public class Submit_IT_Docket {
 
 		if (severity == 1 || severity == 2) {
 			// assigns to a random level one tech
-			System.out.printf("%s %s\n", "Issue ID:", uniqueKey);
+			System.out.printf("%s %s\n", "Ticket ID: ", uniqueKey);
+			System.out.printf("%s %s\n", "Ticket Status: ", status);
 			System.out.printf("%s %s\n", "Issue Description:", description);
 			System.out.printf("%s %s\n", "Issue Severity:", severity);
-			// System.out.printf("%s %s\n", "Issue Reporter:", userName);
+			System.out.printf("%s %s\n", "Issue Reporter:", userName);
 			System.out.printf("%s %s\n", "Assigned Technician:", randomValue);
+			System.out.printf("%s %s\n", "Date Submitted:", date);
 
 		}
 		if (severity == 3 || severity == 4) {
 			// assigns to a random level two tech
-			System.out.printf("%s %s\n", "Issue ID:", uniqueKey);
+			System.out.printf("%s %s\n", "Ticket ID: ", uniqueKey);
+			System.out.printf("%s %s\n", "Ticket Status: ", status);
 			System.out.printf("%s %s\n", "Issue Description:", description);
 			System.out.printf("%s %s\n", "Issue Severity:", severity);
-			// System.out.printf("%s %s\n", "Issue Reporter:", userName);
+			System.out.printf("%s %s\n", "Issue Reporter:", userName);
 			System.out.printf("%s %s\n", "Assigned Technician:", randomValue2);
+			System.out.printf("%s %s\n", "Date Submitted:", date);
 
 		}
 
